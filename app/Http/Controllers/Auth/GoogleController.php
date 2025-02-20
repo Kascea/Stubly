ee3ef883330577ab9341260d97e34fad4f373a36
Kascea <colethomascarlson@gmail.com>
2025-02-19 22:12:20 -0600

    test publish with new db

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -53,6 +53,11 @@ class GoogleController extends Controller
       );
 
       Auth::login($user, true);
+      Log::info('Auth check after login:', [
+        'authenticated' => Auth::check(),
+        'auth_id' => Auth::id(),
+        'session_data' => session()->all(),
+      ]);
       return redirect(route('canvas', absolute: false));
 
     } catch (Exception $e) {
